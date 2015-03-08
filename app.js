7c9dc05cc41cba7b2ad19f549debc0b269fb272d
Enrique Yepez <enriqueyt@gmail.com>
2015-03-08 13:22:30 -0430

    3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var express = require('express'), 
     http    = require('http'),
     app     = express(),
@@ -7,14 +8,30 @@ var express = require('express'),
 
 var general = require('./router/get');
 var func = require('./router/func');
+
+var express    = require('express'), 
+    http       = require('http'),
+	bodyParser = require('body-parser'),
+	path       = require('path'),
+	app        = express();
+
+var general  = require('./router/get'),
+    problema = require('./router/problema');
+
+app.set('port', process.env.PORT || 6969);
+
 	
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', __dirname + '/views');
 
-app.configure(function(){
+app.configure(function() {
 	app.use(express.static(__dirname));
 });
+
 
 app.get("/", function(req,res){
 	res.render("index.jade", 
@@ -54,19 +71,29 @@ app.get("/", function(req,res){
 app.get('/test/:hola', func.algo);
 
 app.get('/pais', general.paises);
+
 app.get('/pais/:id', general.pais);
 
-app.get('/estado/:pais', general.estados);
+app.get('/estados/:pais', general.estados);
 app.get('/estado/:id', general.estado);
 
-app.get('/ciudad/:estado', general.ciudades);
+app.get('/ciudades/:estado', general.ciudades);
 app.get('/ciudad/:id', general.ciudad);
 
-app.get('/municipio/:ciudad', general.municipios);
+app.get('/municipios/:ciudad', general.municipios);
 app.get('/municipio/:id', general.municipio);
 
-server.listen(port, function() {
-    console.log('Express server listening on port ' + port);
+app.get('/subcategorias', general.subcategorias);
+app.get('/subcategoria/:id', general.subcategoria);
+
+app.get('/problemas', problema.buscarProblemas);
+app.post('/problema', problema.crearProblema);
+app.post('/problema/certificar', problema.certificarProblema);
+app.post('/problema/comentar', problema.comentarProblema);
+app.put('/problema/cambiarEstatus', problema.cambiarEstatusProblema);
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 
--- router/problema.js
+++ router/problema.js
@@ -1,41 +1,75 @@
 var connection = require('../config/db'),
     Q          = require('q');
+	
+exports.buscarProblemas = function(req, res) {
+	var sql = 
+		'SELECT ' +
+			'prob.problema AS problema, ' +
+			'prob.longitud AS longitud, ' +
+			'prob.latitud AS latitud, ' +
+			'prob.id_estatus AS id_estatus, ' +
+			'prob.id_municipio AS id_municipio, ' +
+			'prob.id_sub_categoria AS id_sub_categoria, ' +
+			'prob.id_tipo_problema AS id_tipo_problema, ' +
+			'prob.id_usuario AS id_usuario, ' +
+			'img.imagen ' +
+		'FROM ' +
+			'report_it.tb_problema AS prob ' +
+			'LEFT OUTER JOIN ' +
+			'report_it.tb_problema_imagen AS pi ' +
+			'ON prob.id_problema = pi.id_problema ' +
+			'LEFT OUTER JOIN ' +
+			'report_it.tb_imagen AS img ' +
+			'ON pi.id_imagen = img.id_imagen ' +
+		'LIMIT 10;';
+		
+	if (connection) {
+		connection.db.query(
+			sql,
+			null,
+			function(err, result) {
+				if (err) throw err;
+				res.contentType('application/json');
+				res.write(JSON.stringify(result[0]));
+				res.end();
+			});
+		}
+};
+	
+exports.crearProblema = function(req, res) {
 
-exports.crearProblema = function(req, res){
-    	
-	var callback_1 = function() {
+	var callback_1 = function(id_imagen, id_tipo_imagen) {
 		var deferred = Q.defer();
 		var sql = 
 			'SET @id_imagen = -1; ' +
-			'INSERT INTO report_it.imagen (imagen, id_tipo_imagen) ' +
+			'INSERT INTO report_it.tb_imagen (imagen, id_tipo_imagen) ' +
 			'VALUES (?, ?); ' +
 			'SET @id_imagen = LAST_INSERT_ID(); ' +
-			'SELECT @id_imagen;';
+			'SELECT @id_imagen AS id_imagen;';
 			
 		connection.db.query(
 			sql,
-			[req.body.imagen, req.body.tipoImagen],
+			[id_imagen, id_tipo_imagen],
 			function(err, result) {
 				if (err) deferred.reject(err);
 				
-				if (result.length <= 0) {
-					res.contentType('application/json');
-					res.write(JSON.stringify({ msg : 'ERROR - ! - Error buscando datos' }));
-					res.end();
-				}					
+				if (result.length <= 0)
+					deferred.reject(JSON.stringify({ msg : 'ERROR - ! - Error buscando datos' }));				
 				else                   
-					deferred.resolve(result[0]);
+					deferred.resolve(result[3][0]['id_imagen']);
 			}
 		);
+		
+		return deferred.promise;
 	};
 	
-	var callback_2 = function(imagen) {
+	var callback_2 = function(id_imagen) {
 		var sql =
 			'SET @id_problema = -1; ' +
-			'INSERT INTO report_it.tb_problema (problema, latitud, altitud, id_tipo_problema, id_sub_categoria, id_municipio, id_estatus, id_usuario, id_dispositivo, id_grupo) ' +
+			'INSERT INTO report_it.tb_problema (problema, latitud, longitud, id_tipo_problema, id_sub_categoria, id_municipio, id_estatus, id_usuario, id_dispositivo, id_grupo) ' +
 			'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ' +
 			(
-				imagen != null
+				id_imagen != null
 				?	'SET @id_problema = LAST_INSERT_ID(); ' +
 					'INSERT INTO report_it.tb_problema_imagen (id_problema, id_imagen) ' +
 					'VALUES (@id_problema, ?);'
@@ -48,15 +82,15 @@ exports.crearProblema = function(req, res){
 				[
 					req.body.problema,
 					req.body.latitud,
-					req.body.altitud,
-					req.body.tipoProblema,
-					req.body.subCategoria,
-					req.body.municipio,
-					req.body.estatus,
-					req.body.usuario,
-					req.body.dispositivo,
-					req.body.grupo,
-					imagen
+					req.body.longitud,
+					req.body.id_tipo_problema,
+					req.body.id_sub_categoria,
+					req.body.id_municipio,
+					req.body.id_estatus,
+					typeof req.body.id_usuario !== undefined && req.body.id_usuario != null && req.body.id_usuario > 0 ? req.body.id_usuario : null,
+					typeof req.body.id_dispositivo !== undefined && req.body.id_dispositivo != null && req.body.id_dispositivo > 0 ? req.body.id_dispositivo : null,
+					typeof req.body.id_grupo !== undefined && req.body.id_grupo != null && req.body.id_grupo > 0 ? req.body.id_grupo : null,
+					id_imagen
 				],
 				function(err, result) {
 					if (err) throw err;
@@ -67,8 +101,8 @@ exports.crearProblema = function(req, res){
 		}
 	};
 	
-	if (req.body.imagen != null) {
-		callback_1(req.body.imagen).then(
+	if (typeof req.body.imagen !== undefined && req.body.imagen != null && typeof req.body.id_tipo_imagen !== undefined && req.body.id_tipo_imagen != null && req.body.id_tipo_imagen > 0) {
+		callback_1(req.body.imagen, req.body.id_tipo_imagen).then(
 			callback_2,
 			function(err) {
 				throw err;
@@ -89,8 +123,8 @@ exports.certificarProblema = function(req, res) {
 		connection.db.query(
 			sql,
 			[
-				req.body.usuario,
-				req.body.problema
+				req.body.id_usuario,
+				req.body.id_problema
 			],
 			function(err, result) {
 				if (err) throw err;
@@ -105,13 +139,14 @@ exports.comentarProblema = function(req, res) {
 	var sql =
 		'INSERT INTO report_it.tb_problema_comentario (id_problema, id_usuario, comentario) ' +
 		'VALUES (?, ?, '');';
+
 	
 	if (connection) {
 		connection.db.query(
 			sql,
 			[
-				req.body.problema,
-				req.body.usuario,
+				req.body.id_problema,
+				req.body.id_usuario,
 				req.body.comentario
 			],
 			function(err, result) {
@@ -123,7 +158,7 @@ exports.comentarProblema = function(req, res) {
 	}
 };
 
-exports.cambioEstatusProblema = function(req, res) {
+exports.cambiarEstatusProblema = function(req, res) {
 	var sql =
 		'UPDATE report_it.tb_problema ' +
 		'SET id_estatus = ? ' +
@@ -133,8 +168,8 @@ exports.cambioEstatusProblema = function(req, res) {
 		connection.db.query(
 			sql,
 			[
-				req.body.estatus,
-				req.body.problema
+				req.body.id_estatus,
+				req.body.id_problema
 			],
 			function(err, result) {
 				if (err) throw err;
